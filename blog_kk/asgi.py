b63bc51d804864ae541e06a8b3b91bfaa40d0840
dev <devsharma06658@gmail.com>
2026-03-25 22:05:30 +0530

    ASGI fixed and gitignore added

--- blog_kk/asgi.py
+++ blog_kk/asgi.py
@@ -1,16 +1,23 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from core.routing import websocket_urlpatterns  # <-- Ye line add karein
 
+# 1. Sabse pehle settings set karo
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_kk.settings')
 
+# 2. Phir application ko initialize karo (Imports se pehle)
+django_asgi_app = get_asgi_application()
+
+# 3. Ab baaki cheezein import karo
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from core.routing import websocket_urlpatterns 
+
+# 4. Final application setup
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            websocket_urlpatterns  # <-- Khali [] ki jagah ye likhein
+            websocket_urlpatterns
         )
     ),
 })
